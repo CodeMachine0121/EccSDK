2b8044a7e350d23699568a0ff02ded4db6a9df09
james.hsueh <james.hsueh@titansoft.com.sg>
2024-07-24 14:51:21 +0800

    test: add fail test case

--- EccSdkUnitTest/EccSdkTests.cs
+++ EccSdkUnitTest/EccSdkTests.cs
@@ -45,4 +45,36 @@ public class EccSdkTests
 
         Assert.That(result, Is.True);
     }
+
+    [Test]
+    public void should_not_pass()
+    {
+        var sign = ChameleonHashHelper.Sign(new ChameleonHashRequest
+        {
+            KeyPair = _keyPair,
+            Message = "Hello World",
+            Order = _keyPair.PublicKey.Curve.Order,
+            SessionKey = new BigInteger("1234567890")
+        });
+
+        var chameleonHash = ChameleonHashHelper.GetChameleonHash(new ChameleonHashRequest
+        {
+            KeyPair = _keyPair,
+            Message = "Hello World",
+            Order = _keyPair.PublicKey.Curve.Order,
+            SessionKey = new BigInteger("1234567890"),
+            Signature = sign
+        });
+
+        var result = ChameleonHashHelper.Verify(new ChameleonHashRequest
+        {
+            KeyPair = _keyPair,
+            Message = "Hello World123",
+            Order = _keyPair.PublicKey.Curve.Order,
+            Signature = sign
+        }, chameleonHash);
+
+        Assert.That(result, Is.False);
+        
+    }
 }
